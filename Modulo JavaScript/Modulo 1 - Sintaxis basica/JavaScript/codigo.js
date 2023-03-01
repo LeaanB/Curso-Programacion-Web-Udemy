9ee4d62c60509f4a0783878766f09f0ff07980cd
LeaBertero <lea-bertero@hotmail.com>
2023-03-01 12:53:07 -0300

    Modulo Js
    Primera parte

---
+++ Modulo JavaScript/Modulo 1 - Sintaxis basica/JavaScript/codigo.js
@@ -0,0 +1,11 @@
+var cualquiernombre = "cualquiertexto";
+
+//esto es un comentario para una sola linea
+/*esto es un comentario de varias lineas*/
+
+//saludo iniciar de la página
+var saludo = "Estas accediendo al módulo de JavaScript"
+alert(saludo)
+//fin saludo inicial
+
+// document.getElementsByTagName("hola")
