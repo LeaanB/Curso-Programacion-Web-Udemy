d37de2f3e96fef2caa7789d9d6a5c21b6a4687fe
LeaBertero <lea-bertero@hotmail.com>
2023-03-08 16:24:34 -0300

    js
    metodos y propiedades del array

---
+++ Modulo JavaScript/Metodos y propiedades de array/JavaScript/arrays-style.js
@@ -0,0 +1,11 @@
+var articulos = ["libreta", "lapiz", "borrador"];
+
+document.write("<p>");
+
+articulos.length = 1;
+
+document.write(articulos.length + "<br>");
+
+document.write(articulos[1]);
+
+document.write("</p>");
